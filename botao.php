6daaedb66e71819e64d0964f98a3ad9cb15135a5
Gabriel Garcia <95450006+GabrielLeon18@users.noreply.github.com>
2023-06-06 09:04:11 -0300

    Update and rename botão.php to botao.php

--- botao.php
+++ botao.php
@@ -1,22 +1,19 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $POST if ($_SERVER["REQUEST_METHOD"] == "POST") {
   // Coletar os dados do formulário
   $nome = $_POST['nome'];
   $email = $_POST['email'];
   $telefone = $_POST['telefone'];
   $servico = $_POST['servico'];
 
-  // Definir o endereço de e-mail de destino
   $destinatario = "start-tech-empresarial@outlook.com";
 
-  // Montar o corpo do e-mail
   $assunto = "Novo contato do site";
   $mensagem = "Nome: " . $nome . "\n";
   $mensagem .= "E-mail: " . $email . "\n";
   $mensagem .= "Telefone: " . $telefone . "\n";
   $mensagem .= "Serviço desejado: " . $servico . "\n";
 
-  // Enviar o e-mail
   $headers = "From: " . $email . "\r\n" .
              "Reply-To: " . $email . "\r\n" .
              "X-Mailer: PHP/" . phpversion();
