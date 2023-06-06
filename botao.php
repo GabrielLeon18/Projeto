bb82ded9a6f25efdd321e65d6d5165fdaae9663e
Gabriel Garcia <95450006+GabrielLeon18@users.noreply.github.com>
2023-06-06 12:23:58 -0300

    Update botao.php

--- botao.php
+++ botao.php
@@ -1,27 +1,28 @@
 <?php
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
   // Coletar os dados do formulário
-  $nome = $_POST[nome];
-  $email = $_POST[email];
-  $telefone = $_POST[telefone];
-  $servico = $_POST[servico];
+  $nome = $_POST['nome'];
+  $email = $_POST['email'];
+  $telefone = $_POST['telefone'];
+  $servico = $_POST['servico'];
+
+  // Validar os campos (exemplo básico)
+  if (empty($nome) || empty($email) || empty($telefone) || empty($servico)) {
+    echo "Por favor, preencha todos os campos do formulário.";
+    exit;
+  }
+
+  // Filtrar e validar o formato do e-mail
+  $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+  if (!$email) {
+    echo "Por favor, forneça um endereço de e-mail válido.";
+    exit;
+  }
 
   $destinatario = "start-tech-empresarial@outlook.com";
-
   $assunto = "Novo contato do site";
   $mensagem = "Nome: " . $nome . "\n";
   $mensagem .= "E-mail: " . $email . "\n";
   $mensagem .= "Telefone: " . $telefone . "\n";
   $mensagem .= "Serviço desejado: " . $servico . "\n";
-
-  $headers = "From: " . $email . "\r\n" .
-             "Reply-To: " . $email . "\r\n" .
-             "X-Mailer: PHP/" . phpversion();
-
-  if (mail($destinatario, $assunto, $mensagem, $headers)) {
-    echo "Obrigado! Sua mensagem foi enviada com sucesso.";
-  } else {
-    echo "Desculpe, ocorreu um erro ao enviar a mensagem. Por favor, tente novamente mais tarde.";
-  }
-}
-?>
+  />
